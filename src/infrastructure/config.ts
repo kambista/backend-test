d19bdcdb9833b2f6004ffaba65b3eab2d71cbd9f
Cesar Puma <puma.rasec@yahoo.es>
2021-02-22 02:11:09 -0500

    volviendo a mongo de docker

--- src/infrastructure/config.ts
+++ src/infrastructure/config.ts
@@ -1,2 +1,2 @@
-// export const urlMongo = 'mongodb://mongodb/backend_test';
-export const urlMongo = 'mongodb://localhost:27017/backend_test';
+export const urlMongo = 'mongodb://mongodb/backend_test';
+// export const urlMongo = 'mongodb://localhost:27017/backend_test';
